a035a97901d72d1ea78a34a025dd1c73da029613
Okky Hidayana <nearhidayana@Krotchy-MacBook-Air.local>
2026-04-05 20:59:28 +0700

    feat: add Vercel Analytics

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import './globals.css';
 import { Providers } from './providers';
+import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -18,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head />
       <body>
         <Providers>{children}</Providers>
+        <Analytics />
       </body>
     </html>
   );
